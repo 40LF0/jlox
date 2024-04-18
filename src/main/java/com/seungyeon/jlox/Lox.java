c14e82fc2babc6c2ddd3f8588e1904436b0f3b47
SeungYeon <wjdac66@gmail.com>
2024-04-18 23:00:02 +0900

    [feat] Lox : Java interpreter for Lox

---
+++ src/main/java/com/seungyeon/jlox/Lox.java
@@ -0,0 +1,41 @@
+package com.seungyeon.jlox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Lox {
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
+      System.exit(64);
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+  }
+
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+  }
+
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+    for (; ; ) {
+      System.out.println("> ");
+      String line = reader.readLine();
+      if (line == null) break;
+      run(line);
+    }
+  }
+
+  private static void run(String source) {
+    System.out.println(source);
+  }
+}
