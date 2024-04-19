0c6b8878e4dfc7dfe19bf495788bb3986d5d55f2
SeungYeon <wjdac66@gmail.com>
2024-04-19 22:13:33 +0900

    [feat] Handle operator parsing considering whitespace
    - Update operator parsing logic to consider whitespace

--- src/main/java/com/seungyeon/jlox/Scanner.java
+++ src/main/java/com/seungyeon/jlox/Scanner.java
@@ -63,6 +63,13 @@ class Scanner {
         addToken(SLASH);
         break;
 
+      case ' ':
+      case '\r':
+      case '\t':
+        break;
+
+      case '\n': line++; break;
+
       default:
         Lox.error(line, "Unexpected character.");
         break;
