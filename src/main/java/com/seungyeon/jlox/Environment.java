e047f6d87b2fdb5cbdf295d20252080e2293ca0a
SeungYeon <wjdac66@gmail.com>
2024-05-04 21:57:37 +0900

    [feat] add scope feature of variables to Environment.java

--- src/main/java/com/seungyeon/jlox/Environment.java
+++ src/main/java/com/seungyeon/jlox/Environment.java
@@ -3,13 +3,24 @@ package com.seungyeon.jlox;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Environment {
+class Environment {
+  final Environment enclosing;
   private final Map<String, Object> values = new HashMap<>();
+
+  Environment() {
+    enclosing = null;
+  }
+
+  Environment(Environment enclosing) {
+    this.enclosing = enclosing;
+  }
 
   Object get(Token name) {
     if (values.containsKey(name.lexeme)) {
       return values.get(name.lexeme);
     }
+
+    if (enclosing != null) return enclosing.get(name);
 
     throw new RuntimeError(name, "Undefined variable'" + name.lexeme + "'.");
   }
@@ -21,6 +32,11 @@ public class Environment {
   void assign(Token name, Object value) {
     if (values.containsKey(name.lexeme)){
       values.replace(name.lexeme, value);
+      return;
+    }
+
+    if (enclosing != null) {
+      enclosing.assign(name, value);
       return;
     }
 
