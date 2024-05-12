96b21761000f4f58ae44947a79be83670871d401
SeungYeon <wjdac66@gmail.com>
2024-05-13 00:12:18 +0900

    [feat] for loop - internally, desugar for-loop semantic to while-loop semantic

--- src/main/java/com/seungyeon/jlox/Parser.java
+++ src/main/java/com/seungyeon/jlox/Parser.java
@@ -9,7 +9,9 @@ import com.seungyeon.jlox.Expr.Literal;
 import com.seungyeon.jlox.Expr.Unary;
 import com.seungyeon.jlox.Expr.Variable;
 import com.seungyeon.jlox.Stmt.Block;
+import com.seungyeon.jlox.Stmt.While;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Parser {
@@ -70,7 +72,7 @@ class Parser {
   private Expr or() {
     Expr expr = and();
 
-    while(match(OR)) {
+    while (match(OR)) {
       Token operator = previous();
       Expr right = and();
       expr = new Expr.Logical(expr, operator, right);
@@ -82,7 +84,7 @@ class Parser {
   private Expr and() {
     Expr expr = equality();
 
-    while(match(AND)) {
+    while (match(AND)) {
       Token operator = previous();
       Expr right = equality();
       expr = new Expr.Logical(expr, operator, right);
@@ -92,11 +94,49 @@ class Parser {
   }
 
   private Stmt statement() {
+    if (match(FOR)) return forStatement();
     if (match(IF)) return ifStatement();
     if (match(PRINT)) return printStatement();
     if (match(WHILE)) return whileStatement();
     if (match(LEFT_BRACE)) return new Block(block());
     return expressionStatement();
+  }
+
+  private Stmt forStatement() {
+    consume(LEFT_PAREN, "Expect '(' after 'for'.");
+
+    Stmt initializer;
+    if (match(SEMICOLON)) {
+      initializer = null;
+    } else if (match(VAR)) {
+      initializer = varDeclaration();
+    } else {
+      initializer = expressionStatement();
+    }
+
+    Expr condition = null;
+    if (!check(SEMICOLON)) {
+      condition = expression();
+    }
+    consume(SEMICOLON, "Expect ';' after loop condition.");
+
+    Expr increment = null;
+    if (!check(RIGHT_PAREN)) {
+      increment = expression();
+    }
+    consume(RIGHT_PAREN, "Expect ')' after for clauses.");
+    Stmt body = statement();
+
+    if (increment != null) {
+      body = new Stmt.Block(Arrays.asList(body, new Stmt.Expression(increment)));
+    }
+
+    if (condition == null) condition = new Expr.Literal(true);
+    body = new While(condition, body);
+
+    if (initializer != null) body = new Stmt.Block(Arrays.asList(initializer, body));
+
+    return body;
   }
 
   private Stmt ifStatement() {
