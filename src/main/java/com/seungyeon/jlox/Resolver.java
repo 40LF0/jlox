a3f088ac9607034033aedd456cb6df192840d248
SeungYeon <wjdac66@gmail.com>
2024-05-20 23:53:26 +0900

    [feat] resolution error: Declaring multiple variables with the same name in the local scope

--- src/main/java/com/seungyeon/jlox/Resolver.java
+++ src/main/java/com/seungyeon/jlox/Resolver.java
@@ -195,6 +195,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     if (scopes.isEmpty()) return;
 
     Map<String, Boolean> scope = scopes.peek();
+    if(scope.containsKey(name.lexeme)) {
+      Lox.error(name, "Already a variable with this name in this scope.");
+    }
+
     scope.put(name.lexeme, false);
   }
 
